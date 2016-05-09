353b53396661928dcb1079d727d7394ee73c93c7
Ronak Shah <ronakshah725@gmail.com>
2016-05-08 21:18:42 -0500

    Added Vehicle.java Model

---
+++ src/main/java/com/ronak/model/Vehicle.java
@@ -0,0 +1,111 @@
+package com.ronak.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vehicle {
+	  @Id
+	  @GeneratedValue(strategy=GenerationType.IDENTITY)
+	  private Integer id;
+	  
+	  @Column
+	  private boolean checked;
+	  
+	  @Column
+	  private int year;
+
+	  @Column
+	  private String make;
+	  
+	  @Column
+	  private String model;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the checked
+	 */
+	public boolean isChecked() {
+		return checked;
+	}
+
+	/**
+	 * @param checked the checked to set
+	 */
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	/**
+	 * @return the make
+	 */
+	public String getMake() {
+		return make;
+	}
+
+	/**
+	 * @param make the make to set
+	 */
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	/**
+	 * @return the model
+	 */
+	public String getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Vehicle [id=" + id + ", checked=" + checked + ", year=" + year + ", make=" + make + ", model=" + model
+				+ "]";
+	}
+	
+	
+	  
+	  
+
+}
